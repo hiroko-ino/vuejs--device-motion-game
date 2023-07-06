7c83d4a2ea5212b55ac7a990c4856a4470762cb9
hiroko-ino <los73.butterfly.1251.12.15@gmail.com>
2023-07-06 20:31:36 +0900

    デモの書き換え

---
+++ src/components/Demo.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { computed, reactive } from 'vue'
+import { useDeviceMotion } from '@vueuse/core'
+
+const motion = reactive(useDeviceMotion())
+const text = computed(() => JSON.stringify(motion, null, 2))
+</script>
+
+<template>
+  <note class="mb-2">
+    Device Motion:
+  </note>
+  <pre lang="json">{{ text }}</pre>
+  <p>motion.x {{ motion.acceleration?.x }}</p>
+  <div>
+    {{ motion }}
+  </div>
+</template>
