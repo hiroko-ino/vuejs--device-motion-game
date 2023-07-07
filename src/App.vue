af0ba074a0ac2fb1ad926d82d0e3e522cbb6c8f0
hiroko-ino <los73.butterfly.1251.12.15@gmail.com>
2023-07-07 22:33:54 +0900

    スタイリングのベース

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
-import Demo from './components/Demo.vue'
+import Bottle from './components/Bottle.vue'
+import Harisenbon from './components/Harisenbon.vue'
 </script>
 
 <template>
   <div class="wrapper">
-    <Demo />
+    <Bottle>
+      <div class="move">
+        <Harisenbon />
+      </div>
+    </Bottle>
   </div>
 </template>
+
+<style scoped>
+.wrapper {
+  display: grid;
+  place-items: center;
+}
+
+.move {
+  position: absolute;
+  top: calc(50% - 25px);
+  left: calc(50% - 25px);
+}
+</style>
---
+++ src/components/Harisenbon.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="harisenbon"></div>
+</template>
+
+<style scoped>
+.harisenbon {
+  width: 50px;
+  height: 50px;
+  background-color: yellow;
+  border-radius: 50%;
+}
+</style>
