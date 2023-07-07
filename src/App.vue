735ee1e65399f33e7e57a8a20e9160672892deed
hiroko-ino <los73.butterfly.1251.12.15@gmail.com>
2023-07-08 00:29:31 +0900

    ロジックの実装（仮）

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue'
 import Bottle from './components/Bottle.vue'
 import Harisenbon from './components/Harisenbon.vue'
+import { useDeviceMotion } from '@vueuse/core'
+import { HARISENBON_WIDTH, BOTTLE_WIDTH } from './Const'
+
+const { accelerationIncludingGravity } = useDeviceMotion()
+
+/**
+ * ハリセンボンのwrapperのref
+ */
+const moveRef = ref<HTMLDivElement | null>(null)
+
+/**
+ * ハリセンボンのwrapperスタイルのleft
+ */
+const harisenbonLeftStyle = computed(() => {
+  const xTilt = accelerationIncludingGravity.value?.x
+  if (!moveRef.value) return BOTTLE_WIDTH / 2 - HARISENBON_WIDTH / 2
+  const currentPosition = parseInt(moveRef.value.style.left || '0', 10) || 0
+  console.log(currentPosition)
+  const newPosition = currentPosition + (xTilt || 0)
+  return Math.max(0, Math.min(BOTTLE_WIDTH - HARISENBON_WIDTH, newPosition)) // ボトル内に収める
+})
 </script>
 
 <template>
   <div class="wrapper">
     <Bottle>
-      <div class="move">
+      <div ref="moveRef" class="move" :style="{ left: `${harisenbonLeftStyle}px` }">
         <Harisenbon />
       </div>
     </Bottle>
@@ -22,6 +44,5 @@ import Harisenbon from './components/Harisenbon.vue'
 .move {
   position: absolute;
   top: calc(50% - 25px);
-  left: calc(50% - 25px);
 }
 </style>
---
+++ src/Const.ts
@@ -0,0 +1,9 @@
+/**
+ * ハリセンボンのwidth
+ */
+export const HARISENBON_WIDTH = 50
+
+/**
+ * BottleのWidth
+ */
+export const BOTTLE_WIDTH = 200
