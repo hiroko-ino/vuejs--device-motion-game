a8e6529e5bcf639071fcbbe9c9a02418552767e7
hiroko-ino <los73.butterfly.1251.12.15@gmail.com>
2023-07-11 12:17:07 +0900

    算術の変更

--- src/App.vue
+++ src/App.vue
@@ -47,12 +47,12 @@ watch(
     if (harisenbonLeftStyle.value === 0) {
       bottleLeftClashes.value++
       if (!moveRef.value) return
-      moveRef.value.style.left = `${-(rotationRate.value?.beta || 0)}px`
+      moveRef.value.style.left = `${rotationRate.value?.beta || 0}px`
     }
     if (harisenbonLeftStyle.value === BOTTLE_WIDTH - HARISENBON_WIDTH) {
       bottleRightClashes.value++
       if (!moveRef.value) return
-      moveRef.value.style.left = `${BOTTLE_WIDTH - (rotationRate.value?.beta || 0)}px`
+      moveRef.value.style.left = `${BOTTLE_WIDTH - HARISENBON_WIDTH + (rotationRate.value?.beta || 0)}px`
     }
   }
 )
